1c7a6918186fab8ab97ba3e7faa40ea4aeffcee2
wjjmjh <u6269649@anu.edu.au>
2020-11-18 10:56:08 +1100

    MAINT: a more generic react ts class template.

--- src/web2tree/templates/__init__.py
+++ src/web2tree/templates/__init__.py
@@ -1,1 +1,15 @@
 __contributors__ = ["Stephen Ka-Wah Ma"]
+
+react_ts_class_template = """
+class {class_name} extends Component<{props_types}, {states_types}> {
+  {constructor}
+  
+  {body}
+
+  render() {
+    return (
+      {skeleton}
+    );
+  }
+}
+"""
