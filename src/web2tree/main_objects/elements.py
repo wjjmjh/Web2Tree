5d260341ab5a873eb36ae4022a4ccd0aec5a52cb
wjjmjh <u6269649@anu.edu.au>
2020-11-18 10:41:01 +1100

    ENH: enhanced to_skeleton in element.

--- src/web2tree/main_objects/elements.py
+++ src/web2tree/main_objects/elements.py
@@ -2,20 +2,25 @@ from web2tree.utils.data_wrangling import wrap_attr
 
 
 class Element:
-    def __init__(self, type):
-        self.type = type
+    def __init__(self, element_type):
+        self.element_type = element_type
 
     def to_skeleton(self, **attrs):
         content = attrs.pop("content", None)
+        attrs = " ".join(
+            (
+                "{attr_k}={attr_v}".format(
+                    attr_k=str(attr_k), attr_v=wrap_attr(attrs[attr_k])
+                )
+                for attr_k in attrs.keys()
+            )
+        )
         if content is None:
             return "<{type} {attrs}/>".format(
-                type=self.type,
-                attrs=" ".join(
-                    (
-                        "{attr_k}={attr_v}".format(
-                            attr_k=str(attr_k), attr_v=wrap_attr(attrs[attr_k])
-                        )
-                        for attr_k in attrs.keys()
-                    )
-                ),
+                type=self.element_type,
+                attrs=attrs,
+            )
+        else:
+            return "<{type} {attrs}>{content}</{type}>".format(
+                type=self.element_type, content=content, attrs=attrs
             )
