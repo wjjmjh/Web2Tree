97ec7b8f5a94aa73b6a5289971ff53dbda88374a
wjjmjh <u6269649@anu.edu.au>
2020-11-18 11:29:41 +1100

    BUILD: wrap_into_div method in constructions.

--- src/web2tree/core/constructions/skeleton_construction.py
+++ src/web2tree/core/constructions/skeleton_construction.py
@@ -1,4 +1,9 @@
+from web2tree import DIV
 from web2tree.utils.data_wrangling import wrap_attr
+
+
+def wrap_into_div(to_be_wrapped):
+    return construct_skeleton(DIV, content=to_be_wrapped)
 
 
 def construct_skeleton(element_type, **attrs):
@@ -11,12 +16,17 @@ def construct_skeleton(element_type, **attrs):
             for attr_k in attrs.keys()
         )
     )
+    if attrs == "":
+        gap = ""
+    else:
+        gap = " "
     if content is None:
-        return "<{type} {attrs}/>".format(
+        return "<{type}{gap}{attrs}/>".format(
             type=element_type,
+            gap=gap,
             attrs=attrs,
         )
     else:
-        return "<{type} {attrs}>{content}</{type}>".format(
-            type=element_type, content=content, attrs=attrs
+        return "<{type}{gap}{attrs}>{content}</{type}>".format(
+            type=element_type, gap=gap, content=content, attrs=attrs
         )
