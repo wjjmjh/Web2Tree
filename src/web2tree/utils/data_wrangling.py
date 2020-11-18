1bd9f2b9ca63e156a1f00f9b71b4942f1db1b36d
wjjmjh <u6269649@anu.edu.au>
2020-11-18 15:51:00 +1100

    BUILD: two data wrangling methods about nested dicts.

--- src/web2tree/utils/data_wrangling.py
+++ src/web2tree/utils/data_wrangling.py
@@ -1,3 +1,8 @@
+from copy import copy
+
+from web2tree.utils.misc import is_iterable
+
+
 def wrap_str(val):
     if isinstance(val, str):
         return '"{}"'.format(val)
@@ -6,3 +11,32 @@ def wrap_str(val):
 
 def wrap_attr(attr):
     return "".join(("{", wrap_str(attr), "}"))
+
+
+def split_by_arrows(arrowed):
+    return arrowed.replace(" ", "").split("->")
+
+
+def get_val_from_nested_dict(keys, nested_dict):
+    assert (
+        is_iterable(keys) and len(keys) >= 1
+    ), "keys should be iterable and at least having one key."
+
+    if len(keys) == 1:
+        return nested_dict[keys[0]]
+    else:
+        return get_val_from_nested_dict(keys[1:], nested_dict[keys[0]])
+
+
+def set_val_for_nested_dict(keys, v, nested_dict):
+    assert (
+        is_iterable(keys) and len(keys) >= 1
+    ), "keys should be iterable and at least having one key."
+    if len(keys) == 1:
+        nested_dict[keys[0]] = v
+        return nested_dict
+    else:
+        nested_dict[keys[0]] = set_val_for_nested_dict(
+            keys[1:], v, nested_dict[keys[0]]
+        )
+        return nested_dict
